6aae2fb77eb6c1470c9328babfd756410a7298e1
dev-ay <44041117+dev-ay@users.noreply.github.com>
2019-03-03 16:32:32 -0800

    Update GigHub

--- GigHub/GigHub/GigHub/Controllers/Api/GigsController.cs
+++ GigHub/GigHub/GigHub/Controllers/Api/GigsController.cs
@@ -20,6 +20,10 @@ namespace GigHub.Controllers.Api
         {
             var userId = User.Identity.GetUserId();
             var gig = _context.Gigs.Single(g => g.Id == Id && g.ArtistId == userId);
+
+            if (gig.IsCanceled)
+                return NotFound();
+
             gig.IsCanceled = true;
             _context.SaveChanges();
 
--- GigHub/GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/GigHub/Controllers/GigsController.cs
@@ -1,6 +1,7 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
 using Microsoft.AspNet.Identity;
+using System;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
@@ -21,7 +22,7 @@ namespace GigHub.Controllers
         {
             var userId = User.Identity.GetUserId();
             var gigs = _context.Gigs
-                .Where(g => g.ArtistId == userId && g.DateTime > System.DateTime.Now)
+                .Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now && !g.IsCanceled)
                 .Include(g => g.Genre)
                 .ToList();
 
