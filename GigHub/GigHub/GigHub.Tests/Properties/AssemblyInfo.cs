efd30ba6bc9b6b2649354d6b50d15296186dfcf2
dev-ay <44041117+dev-ay@users.noreply.github.com>
2019-03-12 00:09:20 -0700

    Update GigHub

---
+++ GigHub/GigHub/GigHub.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GigHub.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GigHub.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1886e52a-20a5-470f-93fd-9df5f0f1e56d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- GigHub/GigHub/GigHub/Controllers/Api/GigsController.cs
+++ GigHub/GigHub/GigHub/Controllers/Api/GigsController.cs
@@ -4,33 +4,39 @@ using System.Linq;
 using System.Web.Http;
 using GigHub.Core.Models;
 using GigHub.Persistence;
+using GigHub.Core;
 
 namespace GigHub.Controllers.Api
 {
     [Authorize]
     public class GigsController : ApiController
     {
-        private ApplicationDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public GigsController()
+        public GigsController(IUnitOfWork unitOfWork)
         {
-            _context = new ApplicationDbContext();
+            _unitOfWork = unitOfWork;
         }
 
         [HttpDelete]
         public IHttpActionResult Cancel(int Id)
         {
             var userId = User.Identity.GetUserId();
-            var gig = _context.Gigs
-                .Include(g => g.Attendances.Select(a => a.Attendee))
-                .Single(g => g.Id == Id && g.ArtistId == userId);
+            //var gig = _context.Gigs
+            //    .Include(g => g.Attendances.Select(a => a.Attendee))
+            //    .Single(g => g.Id == Id && g.ArtistId == userId);
 
-            if (gig.IsCanceled)
+            var gig = _unitOfWork.Gigs.GetGigWithAttendees(Id);
+
+            if (gig == null || gig.IsCanceled)
                 return NotFound();
+
+            if (gig.ArtistId != userId)
+                return Unauthorized();
 
             gig.Cancel();
 
-            _context.SaveChanges();
+            _unitOfWork.Complete();
 
             return Ok();
         }
